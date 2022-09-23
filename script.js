6ad48961c09dad591dfae98589426cb6c8a2263b
henry <hjbishop@live.co.uk>
2022-09-23 14:33:55 +0900

    clean up UI, js functions. Get basic aspects working

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 
-function startingGrid() {
-    
-let initialGridContainer = document.querySelector(".gridcontainer");
-let defGrid = document.createElement('div');
-defGrid.classList = ('block');
-defGrid.textContent = 'hi';
-defGrid.style.height = '1.95px';
-defGrid.style.width = '1.95px';
-initialGridContainer.appendChild(defGrid);
+starterGrid()
+
+function starterGrid () {
+    let userInput = 16;
+    let input = userInput * userInput;
+    let pixel = 500 / userInput;
+    for (let i = 0; i < input; i++) {
+    let i = addGrid(pixel)
 }
+}
+
+let colours = document.querySelector("div");
+colours.addEventListener("mouseover", () => {
+    
+    event.target.style.backgroundColor = 'black';
+});
+
+let reset = document.querySelector('#reset');
+console.log(reset);
+reset.addEventListener("click", () => {
+colours.style.backgroundColor = 'white';
+
+}
+);
+
+function resetCanvas() {
+    let clear = document.querySelector('div');
+clear.style.backgroundColor = 'white';
+console.log(userInput)
+}
+
 
 let newGrid = document.querySelector('button')
 newGrid.addEventListener("click", event => {
@@ -23,7 +44,7 @@ newGrid.addEventListener("click", event => {
 });
 
 function howMany() {
-    
+
     let userInput = prompt("how many?");
     return userInput
 }
@@ -33,7 +54,7 @@ function addGrid(pixel) {
 let container = document.querySelector(".gridcontainer");
 let grid = document.createElement('div');
 grid.classList = ('block');
-grid.textContent = 'hi';
+grid.textContent = '';
 grid.style.height = pixel + 'px';
 grid.style.width = pixel + 'px';
 container.appendChild(grid);
