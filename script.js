d4dd1f6994c42fb2a39833097966dadf57fc7e86
henry <hjbishop@live.co.uk>
2022-09-26 13:43:38 +0900

    fix window resizing bug with sketch grid

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 starterGrid()
 
 let colours = document.querySelector("div");
 colours.addEventListener("mouseover", () => {
-    event.target.style.backgroundColor = 'black';
+    event.target.classList.replace('block', 'Newblock')
 });
 
 let newGrid = document.querySelector('button')
@@ -15,27 +14,28 @@ newGrid.addEventListener("click", event => {
         let input = userInput * userInput;
         let pixel = 500 / userInput;
         for (let i = 0; i < input; i++) {
-        let i = addGrid(pixel)
+        let i = addGrid(pixel);
+        gridupdate(userInput);
     }}
 });
 
 function starterGrid () {
-    let userInput = 16;
-    let input = userInput * userInput;
-    let pixel = 500 / userInput;
-    for (let i = 0; i < input; i++) {
+let userInput = 16;
+let input = userInput * userInput;
+let pixel = 500 / userInput;
+  for (let i = 0; i < input; i++) {
     let i = addGrid(pixel)
 }
 }
 
 function howMany() {
 
-    let userInput = prompt("Please enter number of tiles(maximum: 100)");
+let userInput = prompt("Please enter number of tiles(maximum: 100)");
     if (userInput > 100) alert("Please try again with 100 or less tiles");
     return userInput
 }
 
-function addGrid(pixel) {
+function addGrid(pixel, userInput) {
 
 let container = document.querySelector(".gridcontainer");
 let grid = document.createElement('div');
@@ -46,12 +46,19 @@ grid.style.width = pixel + 'px';
 container.appendChild(grid);
 }
 
+function gridupdate(userInput) {
+
+let container = document.querySelector(".gridcontainer");
+container.style.setProperty("grid-template-columns", `repeat(${userInput}, 2fr)`);
+container.style.setProperty("grid-template-rows", `repeat(${userInput}, 2fr)`);
+}
+
 function deleteChild() {
 
-    let e = document.querySelector(".gridcontainer");
-    let child = e.lastElementChild; 
-    while (child) {
-        e.removeChild(child);
-        child = e.lastElementChild;
-    }
+let e = document.querySelector(".gridcontainer");
+let child = e.lastElementChild; 
+  while (child) {
+    e.removeChild(child);
+    child = e.lastElementChild;
+  }
 }
